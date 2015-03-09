837f8fcb7a04e6182affa38eaa9715e8c1d5cd9c
Charles Crichton <charles.crichton@ndm.ox.ac.uk>
2015-03-09 12:19:31 +0000

    Filtereing and sorting for new GEL fields

--- core/src/main/java/org/akaza/openclinica/dao/managestudy/FindSubjectsFilter.java
+++ core/src/main/java/org/akaza/openclinica/dao/managestudy/FindSubjectsFilter.java
@@ -1,17 +1,45 @@
 package org.akaza.openclinica.dao.managestudy;
 
 import org.akaza.openclinica.bean.core.SubjectEventStatus;
+import org.akaza.openclinica.i18n.util.ResourceBundleProvider;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.print.DocFlavor;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
 public class FindSubjectsFilter implements CriteriaCommand {
 
+    //protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
+
     List<Filter> filters = new ArrayList<Filter>();
     HashMap<String, String> columnMapping = new HashMap<String, String>();
 
+    public final static String BESPOKE_FAMILY_ID = "familyId";
+    public final static String BESPOKE_SURNAME = "surname";
+    public final static String BESPOKE_FORENAMES = "forenames";
+    public final static String BESPOKE_DOB = "dob";
+    public final static String BESPOKE_NHS_NUMBER = "nhsNumber";
+    public final static String BESPOKE_HOSPITAL_NUMBER = "hospitalNumber";
+    public final static String BESPOKE_SLF_DOWNLOADED = "slfDownloaded";
+
+    /*
+    <query>
+    <name>getWithFilterAndSort</name>
+    <sql>SELECT DISTINCT(ss.*), ST.unique_identifier
+    FROM study_subject ss LEFT JOIN study_event se ON ss.study_subject_id = se.study_subject_id,STUDY ST,SUBJECT S
+            where
+    SS.SUBJECT_ID=S.SUBJECT_ID
+    AND SS.study_id = ST.study_id
+                      AND (ST.study_id=? or ST.parent_study_id=?)
+    </sql>
+    </query>
+    */
     public FindSubjectsFilter() {
         columnMapping.put("studySubject.label", "ss.label");
         columnMapping.put("studySubject.status", "ss.status_id");
@@ -19,7 +47,13 @@ public class FindSubjectsFilter implements CriteriaCommand {
         columnMapping.put("enrolledAt", "ST.unique_identifier");
         columnMapping.put("studySubject.secondaryLabel", "ss.secondary_label");
         columnMapping.put("subject.charGender", "s.gender");
-
+        columnMapping.put(BESPOKE_FAMILY_ID, "ss.gel_family_id");
+        columnMapping.put(BESPOKE_SURNAME, "ss.gel_surname");
+        columnMapping.put(BESPOKE_FORENAMES, "ss.gel_forenames");
+        columnMapping.put(BESPOKE_NHS_NUMBER, "ss.gel_nhs_number");
+        columnMapping.put(BESPOKE_HOSPITAL_NUMBER, "ss.gel_hospital_number");
+        columnMapping.put(BESPOKE_SLF_DOWNLOADED, "ss.gel_slf_downloaded");
+        columnMapping.put(BESPOKE_DOB, "s.date_of_birth");
     }
 
     public void addFilter(String property, Object value) {
@@ -31,6 +65,7 @@ public class FindSubjectsFilter implements CriteriaCommand {
         for (Filter filter : filters) {
             theCriteria += buildCriteria(criteria, filter.getProperty(), filter.getValue());
         }
+        //logger.info("theCriteria = "+theCriteria);
         return theCriteria;
     }
 
@@ -65,14 +100,53 @@ public class FindSubjectsFilter implements CriteriaCommand {
                         + " ) ";
 
             }
+            /**
+            else if (property.equals(BESPOKE_FAMILY_ID)) {
+                criteria = criteria + " and ";
+                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER('%\\[" + value.toString() + "\\]%')" + " ";
+            }
+            else if (property.equals(BESPOKE_SURNAME)) {
+                criteria = criteria + " and ";
+                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER('" + value.toString() + ",%')" + " ";
+            }
+            else if (property.equals(BESPOKE_FORENAMES)) {
+                criteria = criteria + " and ";
+                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER('%, " + value.toString() + " \\[%')" + " ";
+            }
+            else if (property.equals(BESPOKE_NHS_NUMBER)) {
+                criteria = criteria + " and ";
+                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER('%\\] " + value.toString() + "% %')" + " ";
+            }
+             */
+
+            else if (property.equals(BESPOKE_DOB)) {
+                criteria = criteria + " and ";
+                criteria = criteria + " to_char(" + columnMapping.get(property) +", 'DD/MM/YYYY') like '%"+ value.toString() +"%' ";
+            }
+
+            else if (property.equals(BESPOKE_SLF_DOWNLOADED)) {
+                criteria = criteria + " and ";
+
+                Boolean testValue = false;
+                if (value instanceof String) {
+                    testValue = Boolean.valueOf((String) value);
+                }
+
+                criteria = criteria + " UPPER(cast(" + columnMapping.get(property) + " AS VARCHAR)) like UPPER('%" + value.toString() + "%')  ";
+
+                //criteria = criteria + " " + columnMapping.get(property) + " IS "+testValue.toString().toUpperCase()+"  ";
+            }
 
             else {
                 criteria = criteria + " and ";
                 criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER('%" + value.toString() + "%')" + " ";
             }
+
         }
         return criteria;
     }
+
+
 
     private static class Filter {
         private final String property;
--- core/src/main/java/org/akaza/openclinica/dao/managestudy/FindSubjectsSort.java
+++ core/src/main/java/org/akaza/openclinica/dao/managestudy/FindSubjectsSort.java
@@ -1,20 +1,50 @@
 package org.akaza.openclinica.dao.managestudy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class FindSubjectsSort implements CriteriaCommand {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
+
     List<Sort> sorts = new ArrayList<Sort>();
     HashMap<String, String> columnMapping = new HashMap<String, String>();
 
     public FindSubjectsSort() {
+        /*
         columnMapping.put("studySubject.label", "label");
         columnMapping.put("studySubject.status", "status_id");
         columnMapping.put("enrolledAt", "ST.unique_identifier");
         columnMapping.put("studySubject.oid", "oc_oid");
         columnMapping.put("studySubject.secondaryLabel", "secondary_label");
+
 //        columnMapping.put("subject.charGender", "gender");
+        //columnMapping.put("subject.charGender", "s.gender");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_FAMILY_ID, "gel_family_id");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_SURNAME, "gel_surname");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_FORENAMES, "gel_forenames");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_NHS_NUMBER, "gel_nhs_number");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_HOSPITAL_NUMBER, "gel_hospital_number");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_SLF_DOWNLOADED, "gel_slf_downloaded");
+*/
+        columnMapping.put("studySubject.label", "ss.label");
+        columnMapping.put("studySubject.status", "ss.status_id");
+        columnMapping.put("studySubject.oid", "ss.oc_oid");
+        columnMapping.put("enrolledAt", "ST.unique_identifier");
+        columnMapping.put("studySubject.secondaryLabel", "ss.secondary_label");
+        columnMapping.put("subject.charGender", "s.gender");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_FAMILY_ID, "ss.gel_family_id");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_SURNAME, "ss.gel_surname");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_FORENAMES, "ss.gel_forenames");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_NHS_NUMBER, "ss.gel_nhs_number");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_HOSPITAL_NUMBER, "ss.gel_hospital_number");
+        columnMapping.put(FindSubjectsFilter.BESPOKE_SLF_DOWNLOADED, "ss.gel_slf_downloaded");
+        //columnMapping.put(FindSubjectsFilter.BESPOKE_DOB, "s.date_of_birth");
+
     }
 
     public void addSort(String property, String order) {
@@ -35,7 +65,7 @@ public class FindSubjectsSort implements CriteriaCommand {
             }
 
         }
-
+        logger.info("theCriteria = "+theCriteria);
         return theCriteria;
     }
 
